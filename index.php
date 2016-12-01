77871f41afb63044f58321ffeb91b111f0c7d8ab
riegersn <riegersn@gmail.com>
2016-12-01 13:47:14 -0500

    pagination, single posts

--- index.php
+++ index.php
@@ -4,12 +4,23 @@
 
 		<div class="col-sm-8 blog-main">
 
-			<?php 
+			<?php
 			if ( have_posts() ) : while ( have_posts() ) : the_post();
-  	
+
 				get_template_part( 'content', get_post_format() );
-  
-			endwhile; endif; 
+
+			endwhile;
+			?>
+
+			<nav>
+				<ul class="pager">
+					<li><?php next_posts_link( 'Previous' ); ?></li>
+					<li><?php previous_posts_link( 'Next' ); ?></li>
+				</ul>
+			</nav>
+
+			<?php
+				endif;
 			?>
 
 		</div> <!-- /.blog-main -->
