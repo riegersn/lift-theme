0822188050b641e4161fba52a89fa884fd2b0382
riegersn <riegersn@gmail.com>
2016-12-01 14:46:16 -0500

    featured images/options

--- functions.php
+++ functions.php
@@ -8,3 +8,57 @@ function lift_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'lift_scripts' );
+
+
+// Add Google Fonts
+function lift_google_fonts() {
+				wp_register_style('OpenSans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
+				wp_enqueue_style( 'OpenSans');
+		}
+
+add_action('wp_print_styles', 'lift_google_fonts');
+
+// WordPress Titles
+add_theme_support( 'title-tag' );
+
+// Support Featured Images
+add_theme_support( 'post-thumbnails' );
+
+// Custom settings
+function custom_settings_add_menu() {
+  add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99);
+}
+add_action( 'admin_menu', 'custom_settings_add_menu' );
+
+// Create Custom Global Settings
+function custom_settings_page() { ?>
+  <div class="wrap">
+    <h1>Custom Settings</h1>
+    <form method="post" action="options.php">
+       <?php
+           settings_fields('section');
+           do_settings_sections('theme-options');
+           submit_button();
+       ?>
+    </form>
+  </div>
+<?php }
+
+// Twitter
+function setting_twitter() { ?>
+  <input type="text" name="twitter" id="twitter" value="<?php echo get_option('twitter'); ?>" />
+<?php }
+
+function setting_facebook() { ?>
+  <input type="text" name="facebook" id="facebook" value="<?php echo get_option('facebook'); ?>" />
+<?php }
+
+function custom_settings_page_setup() {
+  add_settings_section('section', 'All Settings', null, 'theme-options');
+  add_settings_field('twitter', 'Twitter URL', 'setting_twitter', 'theme-options', 'section');
+  add_settings_field('facebook', 'Facebook URL', 'setting_facebook', 'theme-options', 'section');
+
+	register_setting('section', 'twitter');
+  register_setting('section', 'facebook');
+}
+add_action( 'admin_init', 'custom_settings_page_setup' );
--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -8,21 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title><?php echo get_bloginfo( 'name' ); ?></title>
-
-    <!-- Core CSS -->
-    <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"> -->
-
-    <!-- Custom styles for this template -->
-    <link href="<?php bloginfo('template_directory');?>/blog.css" rel="stylesheet">
-
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <?php wp_head(); ?>
   </head>
 
   <body>
