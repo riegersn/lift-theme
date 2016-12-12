84288340a479ab3173e0fd8e6c100cf9af7fe640
riegersn <riegersn@gmail.com>
2016-12-12 14:30:29 -0500

    fix sb start pos

--- js/shareable.js
+++ js/shareable.js
@@ -1,9 +1,11 @@
 jQuery(window).scroll(function(){
+
     var sb_content_wrap = jQuery('.sb-content-wrap'),
         scroll_top = jQuery(window).scrollTop(),
+        bp_title_top = jQuery('.bp-container > .bp-title').position().top - 33,
         banner_top = jQuery('#subscribe-banner').position().top - sb_content_wrap.height() - 55;
 
-    if (scroll_top >= 760 && scroll_top < banner_top) {
+    if (scroll_top >= bp_title_top && scroll_top < banner_top) {
       sb_content_wrap.removeClass('sb-fixed-hidden');
        sb_content_wrap.addClass('sb-fixed-content-wrap');
     }
