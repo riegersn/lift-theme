fbf36ed0d7252848a9e9ab028218187e52d2264d
Shawn Rieger <riegersn@gmail.com>
2016-12-19 12:28:02 -0500

    nice!

--- header.php
+++ header.php
@@ -1,33 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-	 <meta charset="utf-8">
-	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	 <meta name="viewport" content="width=device-width, initial-scale=1">
-	 <meta name="description" content="">
-	 <meta name="author" content="">
-	 <?php wp_head(); ?>
-</head>
-
-	 <body>
-
-			<div class="tlg-header-container">
-				 <nav class="tlg-header-wrap">
-						<div class="hd-logo">
-							 <a href="<?php bloginfo( 'wpurl' );?>">
-								<img class="hd-logo-img" src="http://localhost:8888/wp-content/uploads/2016/12/ellipse4230-7.png">
-							 </a>
-						</div>
-						<div class="tlg-header-menu">
-								<ul>
-										<li>
-												<a href="<?php bloginfo( 'wpurl' );?>">Blog</a>
-										</li>
-										<?php wp_list_pages( '&title_li=' ); ?>
-								</ul>
-						</div>
-				 </nav>
-			</div>
-
-			<div class="content-container">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<div class="header-main">
+			<nav class="header-content">
+				<div class="header-logo-container">
+					<a href="<?php bloginfo( 'wpurl' );?>">
+						<img class="header-logo" src="http://localhost:8888/wp-content/uploads/2016/12/logo-dark-60h.png">
+					</a>
+				</div>
+				<div class="header-menu">
+					<ul>
+						<li>
+							<a href="<?php bloginfo( 'wpurl' );?>">Blog</a>
+						</li>
+						<?php wp_list_pages( '&title_li=' ); ?>
+					</ul>
+				</div>
+			</nav>
+		</div>
+		<div class="content-container">
