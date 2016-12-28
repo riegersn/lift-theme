d1327d785fbacbc5c715e24cbea18eab98f9e8c5
riegersn <riegersn@gmail.com>
2016-12-27 21:04:51 -0500

    header refactor

--- header.php
+++ header.php
@@ -9,31 +9,42 @@
 		<?php wp_head(); ?>
 	</head>
 	<body>
-		<div class="m-header">
 
-			<nav class="h-wrap">
-				<div class="h-logo">
-					<a href="<?php bloginfo( 'wpurl' );?>">
-						<img src="<?php echo get_template_directory_uri(); ?>/img/lyftguy_logo.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/img/lyftguy_logo_fallback.png'">
-					</a>
-				</div>
-				<div class="h-menu">
-					<a class="mobile-only h-three-bars" href="#">
-						<i class='fa fa-bars' aria-hidden='true'></i>
-					</a>
-					<ul class="h-menu-ul">
-						<li><a href="<?php bloginfo( 'wpurl' );?>">Blog</a></li>
-						<?php wp_list_pages( '&title_li=' ); ?>
-						<!-- TODO: must have proper nav menu here so that it can be modified from wordpress. -->
-						<a class="mobile-only drive-now buttons-border" href="#">START DRIVING NOW</a>
-					</ul>
+	<header>
+		<nav class="h-wrap">
 
-				</div>
-				<script type="text/javascript">
-					jQuery('.h-three-bars').click(function(){
-						jQuery('.h-menu-ul').slideToggle('fast');
-					});
-				</script>
-			</nav> <!-- /h-logo-wrap -->
-		</div> <!-- /main-header -->
-		<div class="m-content">
+			<!-- the logo -->
+			<div class="h-logo">
+				<a href="<?php bloginfo( 'wpurl' );?>">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/lyftguy_logo.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/img/lyftguy_logo_fallback.png'">
+				</a>
+			</div>
+
+			<!-- the main menu -->
+			<div class="nav-menu nav-item-highlight">
+				<?php wp_nav_menu( array(
+					'menu' => 'main-nav-menu',
+					'container_class' => 'nav-menu-ul' ) );
+					?>
+			</div>
+
+			<!-- the three bars -->
+			<div class="mobile-only h-three-bars">
+				<a href="#">
+					<i class='fa fa-bars' aria-hidden='true'></i>
+				</a>
+			</div>
+
+			<!-- tiny menu -->
+			<div class="mobile-nav-menu">
+				<?php wp_nav_menu( array(
+					'menu' => 'main-nav-menu',
+					'container_class' => 'nav-mobile-menu-ul' ) );
+					?>
+			</div>
+
+		</nav>
+	</header>
+
+	<!-- start content -->
+	<div class="m-content">
