9427f4a46ca62ac8e396b326796b27205c04a5d8
Shawn Rieger <riegersn@gmail.com>
2016-12-28 18:04:19 -0500

    added fixme

--- js/lift.js
+++ js/lift.js
@@ -3,5 +3,5 @@ jQuery(document).ready(function($) {
 
 	// mobile nav menu, slide on click
 	$('.mobile-bars').click(function(){ $('.header-menu').slideToggle(); });
-
+	// fixme: when resized above mobile switch, if toggle was triggered menu display is none
 });
