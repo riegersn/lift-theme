bcd103c1d1942e48685c08a943b2470fd26e15c6
Shawn Rieger <riegersn@gmail.com>
2016-12-28 17:20:23 -0500

    header implement slideToggle

--- js/lift.js
+++ js/lift.js
@@ -1,6 +1,7 @@
 
+jQuery(document).ready(function($) {
 
+	// mobile nav menu, slide on click
+	$('.mobile-bars').click(function(){ $('nav').slideToggle(); });
 
-jQuery('.h-three-bars').click(function(){
-	jQuery('.nav-menu-ul').slideToggle('fast');
 });
