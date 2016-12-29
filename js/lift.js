132992ace7e46f91369bf60f634ff1c8b5525097
riegersn <riegersn@gmail.com>
2016-12-29 01:35:55 -0500

    switch to slim-nav when scroll reaches content

--- js/lift.js
+++ js/lift.js
@@ -4,4 +4,20 @@ jQuery(document).ready(function($) {
 	// mobile nav menu, slide on click
 	$('.mobile-bars').click(function(){ $('.header-menu').slideToggle(); });
 	// fixme: when resized above mobile switch, if toggle was triggered menu display is none
+	// switch to slim-nav once scroll reaches content
+	$(window).scroll(function() {
+    	var header = $('.header'),
+    		content = $('.post-title').position().top - 138,
+			scroll_top = Math.round($(window).scrollTop());
+
+		if (scroll_top >= content) {
+			if ( !header.hasClass('slim-nav') )
+				header.addClass('slim-nav');
+		}
+		else {
+			if ( header.hasClass('slim-nav') )
+				header.removeClass('slim-nav');
+		}
+    });
+
 });
