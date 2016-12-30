3b463d317d36f9fcb15b0b383e1a251930321930
riegersn <riegersn@gmail.com>
2016-12-30 09:16:06 -0500

    moved to new loc

---
+++ subscribe.php
@@ -0,0 +1,9 @@
+
+<!-- 		<div class="c-row c-header">
+			<img class="c-header" src="<?php echo get_template_directory_uri(); ?>/img/welcome_76.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/img/welcome_76_fallback.png'">
+		</div> -->
+
+		<div class="c-row c-panel">
+			<p>Hi, I'm Shawn. I'm a husband, father and a Lyft driver in the NJ area. After driving with both Uber &amp; Lyft for some time, I found it to be fun, exciting and financially bennificial.</p>
+			Want a bonus? Sign up now to <a href="#" title="Start driving now">get a few hundred extra bucks</a>.</p>
+		</div>
