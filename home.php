648c0fb42056ca42e693781dd4d334e28b2bfa5a
riegersn <riegersn@gmail.com>
2016-12-30 09:15:56 -0500

    get rid of this

--- home.php
+++ home.php
@@ -6,16 +6,6 @@
 	<div class="c-column">
 
 		<div class="c-row c-header">
-			<img class="c-header" src="<?php echo get_template_directory_uri(); ?>/img/welcome_76.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/img/welcome_76_fallback.png'">
-		</div>
-
-		<div class="c-row c-panel">
-			<p>Hi, I'm Shawn. I'm a husband, father and a Lyft driver in the NJ area. After driving with both Uber &amp; Lyft for some time, I found it to be exciting, stimulating and financially bennificial. As I began to see weekly payouts ranging from $400-600, then nearly $800 a week, I knew this driving thing was real.</p>
-			<p></p>
-			<p></p>
-		</div>
-
-		<div class="c-row c-header">
 			<img class="c-header" src="<?php echo get_template_directory_uri(); ?>/img/the_blog_140.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/img/the_blog_140_fallback.png'">
 		</div>
 
