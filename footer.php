efa3470568276276280579e15f8fd3a002dbbc0c
Shawn Rieger <riegersn@gmail.com>
2016-12-31 12:54:42 -0500

    added alt img to footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
     </div><!-- /.m-container -->
 
     <footer class="m-footer">
-        <p>The LyftGuy</p>
-        <p>
+        <p><img src="<?php echo get_template_directory_uri(); ?>/img/lyftguy_text_75.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/img/lyftguy_text_75_fallback.png'"></p>
+        <p>Copyright © 2016 The LyftGuy. All Rights Reserved.</p>
+<!--         <p>
             <a href="https://www.facebook.com/theLyftGuy/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
             <a href="http://twitter.com/thelyftguy_net" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-        </p>
-        <p>Copyright © 2016 The LyftGuy. All Rights Reserved.</p>
+        </p> -->
     </footer>
 
     <?php wp_footer(); ?>
