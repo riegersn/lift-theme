9fc6891dcfb7cf68a4b4ea271fb01f7e0a55b8d0
Shawn Rieger <riegersn@gmail.com>
2017-01-02 19:03:27 -0500

    remove social links for now

--- footer.php
+++ footer.php
@@ -3,10 +3,6 @@
     <footer class="m-footer">
         <p><?php echo print_svg_image('footer-logo', 'lyftguy_text_75.svg', 'lyftguy_text_75.png'); ?></p>
         <p>Copyright © 2016 The LyftGuy. All Rights Reserved.</p>
-        <p>
-            <a href="https://www.facebook.com/theLyftGuy/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            <a href="http://twitter.com/thelyftguy_net" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-        </p>
     </footer>
 
     <?php wp_footer(); ?>
