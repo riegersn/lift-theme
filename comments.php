3813f852e507ecb18ad7b38e48a7b287d8d721a9
Shawn Rieger <riegersn@gmail.com>
2017-01-05 13:22:17 -0500

    tidy, comments

--- comments.php
+++ comments.php
@@ -2,28 +2,30 @@
 	return;
 } ?>
 
-<div class="cm-container">
-	<?php if ( have_comments() ) : ?>
-		<h3 class="comments-title">
-			<?php
-			printf( _nx( 'One awesome comment.', '%1$s awesome comments.', get_comments_number(), 'comments title'),
-				number_format_i18n( get_comments_number() ), get_the_title() );
-			?>
-		</h3>
-		<ul class="comment-list">
-			<?php
-			wp_list_comments( array(
-				'walker' => new comment_walker,
-				'short_ping'  => true,
-				'avatar_size' => 75,
-			) );
-			?>
-		</ul>
-	<?php endif; ?>
-	<?php if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-		<p class="no-comments">
-			<?php _e( 'Comments are closed.' ); ?>
-		</p>
-	<?php endif; ?>
-	<?php comment_form(); ?>
+<div class="content-container">
+	<div class="comments">
+		<?php if ( have_comments() ) : ?>
+			<h3 class="comments-title">
+				<?php
+				printf( _nx( 'One awesome comment.', '%1$s awesome comments.', get_comments_number(), 'comments title'),
+					number_format_i18n( get_comments_number() ), get_the_title() );
+				?>
+			</h3>
+			<ul class="comment-list">
+				<?php
+				wp_list_comments( array(
+					'walker' => new comment_walker,
+					'short_ping'  => true,
+					'avatar_size' => 75,
+				) );
+				?>
+			</ul>
+		<?php endif; ?>
+		<?php if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+			<p class="no-comments">
+				<?php _e( 'Comments are closed.' ); ?>
+			</p>
+		<?php endif; ?>
+		<?php comment_form(); ?>
+	</div>
 </div>
--- content-single.php
+++ content-single.php
@@ -1,12 +1,13 @@
 
 <div class="post-featured-image" style="background-image: url('<?php the_post_thumbnail_url( 'full' ); ?>')">
 </div>
-
-<div class="post-content">
-	<?php get_template_part('shareable') ?>
-	<p class="post-meta">/theLyftGuy/<?php the_date(); ?></a></p>
-	<h1 class="post-title"><?php the_title(); ?></h1>
-	<?php the_content(); ?>
+<div class="content-container">
+	<div class="post-content">
+		<?php get_template_part('shareable') ?>
+		<p class="post-meta">/theLyftGuy/<?php the_date(); ?></a></p>
+		<h1 class="post-title"><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+	</div>
 </div>
 
 </div>
