deb9b527a5fed92c06d5569fc69c74f924968d0d
Shawn Rieger <riegersn@gmail.com>
2017-01-06 16:43:12 -0500

    check for thumb

--- content-single.php
+++ content-single.php
@@ -1,6 +1,9 @@
 
-<div class="post-featured-image" style="background-image: url('<?php the_post_thumbnail_url( 'full' ); ?>')">
-</div>
+<?php if(has_post_thumbnail()) { ?>
+	<div class="post-featured-image" style="background-image: url('<?php the_post_thumbnail_url( 'full' ); ?>')">
+	</div>
+<?php } ?>
+
 <div class="content-container">
 	<div class="post-content">
 		<?php get_template_part('shareable') ?>
