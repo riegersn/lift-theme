9fc66f551cb9b817c5e61d5aeae4306846f2d283
Shawn Rieger <riegersn@gmail.com>
2017-01-06 17:08:34 -0500

    creating base containers

--- home.php
+++ home.php
@@ -5,34 +5,33 @@
 <m-content> -->
 
 	<div class="home-featured-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/home.jpg')" >
+	</div>
+
+	<div class="content-container the-blog">
+		<img class="c-header" src="<?php echo get_template_directory_uri(); ?>/img/the_blog_140.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/img/the_blog_140_fallback.png'">
+	</div>
+
+
+	<div class="content-container entry-post-grid transparent">
+
+		<?php
+		if ( have_posts() ) : while ( have_posts() ) : the_post();
+			get_template_part( 'content', get_post_format() );
+			endwhile;
+		?>
+
+		<!--
+		<ul class="pager">
+				<li><?php next_posts_link( 'Previous' ); ?></li>
+				<li><?php previous_posts_link( 'Next' ); ?></li>
+			</ul>
+		-->
+
+		<?php
+			endif;
+		?>
 
 	</div>
-	<div class="c-column">
-
-		<div class="c-row c-header the-blog">
-			<img class="c-header" src="<?php echo get_template_directory_uri(); ?>/img/the_blog_140.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/img/the_blog_140_fallback.png'">
-		</div>
-
-		<div class="c-post-panel">
-
-			<?php
-			if ( have_posts() ) : while ( have_posts() ) : the_post();
-				get_template_part( 'content', get_post_format() );
-				endwhile;
-			?>
-
-			<!--
-			<ul class="pager">
-					<li><?php next_posts_link( 'Previous' ); ?></li>
-					<li><?php previous_posts_link( 'Next' ); ?></li>
-				</ul>
-			-->
-
-			<?php
-				endif;
-			?>
-
-		</div>
 
 	</div> <!-- /.c-column -->
 </div> <!-- /.m-content -->
