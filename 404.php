f0c6676af5246a52efdac240e6ff1855f179260c
Shawn Rieger <riegersn@gmail.com>
2017-01-06 15:17:21 -0500

    added 404

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="special-404">
+
+	<h1>404</h1>
+	<p>Nothing to see here. <a href="http://localhost:8080">Check out our most recent posts.</a></p>
+
+</div>
+
+<?php get_footer(); ?>
