b2ab57902a332b4bbbe29a0041f12b4cf0e82807
Shawn Rieger <riegersn@gmail.com>
2017-01-14 17:45:59 -0500

    subscribe page

--- subscribe.php
+++ subscribe.php
@@ -1,9 +1,30 @@
+<?php
+/*
+Template Name: Subscribe
+*/
+get_header(); ?>
 
-<!-- 		<div class="c-row c-header">
-			<img class="c-header" src="<?php echo get_template_directory_uri(); ?>/img/welcome_76.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/img/welcome_76_fallback.png'">
-		</div> -->
+<div class="content-container">
 
-		<div class="c-row c-panel">
-			<p>Hi, I'm Shawn. I'm a husband, father and a Lyft driver in the NJ area. After driving with both Uber &amp; Lyft for some time, I found it to be fun, exciting and financially bennificial.</p>
-			Want a bonus? Sign up now to <a href="#" title="Start driving now">get a few hundred extra bucks</a>.</p>
+	<h1>Subscribe!</h1>
+
+	<form action="//thelyftguy.us14.list-manage.com/subscribe/post?u=fa32ac29ec19b61d8e3b4ea47&amp;id=3e8b46ba4a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		<h2 class="welcome-header">Welcome to the theLyftGuy!</h2>
+		<div class="welcome-copy">
+			<p>I'm the LyftGuy, otherwise known as Shawn. Welcome to my first blog! I'm a husband, father and lover of all things Lyft!</p>
+			<p><input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="your name"></p>
+			<p><input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="your@email.com"></p>
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_fa32ac29ec19b61d8e3b4ea47_3e8b46ba4a" tabindex="-1" value=""></div>
+			<p><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button welcome-button"></p>
 		</div>
+	</form>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+		<!-- <a class="button">Close</a> -->
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+
+</div><!-- #container -->
+
+<?php get_footer(); ?>
