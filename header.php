1d411bab4e4a1ad332b62352ec88046ae06ee3bd
riegersn <riegersn@gmail.com>
2017-01-14 11:04:50 -0500

    move welcome

--- header.php
+++ header.php
@@ -29,15 +29,22 @@
 		</div>
 	</header>
 
-	<div class="welcome-wrap">
-		<h2 class="welcome-header">Welcome to the theLyftGuy!</h2>
-		<div class="welcome-copy">
-			<p>I'm the LyftGuy, otherwise known as Shawn. Welcome to my first blog! I'm a husband, father and lover of all things Lyft!</p>
-			<p><input type="name" name="singup_name" value="" placeholder="your name"></p>
-			<p><input type="email" name="singup_email" value="" placeholder="your@email.com"></p>
-			<p><a class="button welcome-button" href="#">Subscribe</a></p>
-		</div>
-	</div>
-
 	<!-- start content -->
 	<div class="m-content">
+		<div class="welcome-wrap">
+			<form action="//thelyftguy.us14.list-manage.com/subscribe/post?u=fa32ac29ec19b61d8e3b4ea47&amp;id=3e8b46ba4a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<h2 class="welcome-header">Welcome to the theLyftGuy!</h2>
+				<div class="welcome-copy">
+					<p>I'm the LyftGuy, otherwise known as Shawn. Welcome to my first blog! I'm a husband, father and lover of all things Lyft!</p>
+					<p><input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="your name"></p>
+					<p><input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="your@email.com"></p>
+					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_fa32ac29ec19b61d8e3b4ea47_3e8b46ba4a" tabindex="-1" value=""></div>
+					<p><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button welcome-button"></p>
+				</div>
+			</form>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+		</div>
