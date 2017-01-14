ea1f60c351a02d7b644bf6a69196c4d12e71a7c3
Shawn Rieger <riegersn@gmail.com>
2017-01-14 17:47:15 -0500

    finishing up footer

--- footer.php
+++ footer.php
@@ -3,6 +3,8 @@
     <footer class="m-footer">
         <p><?php echo print_svg_image('footer-logo', 'lyftguy_text_75.svg', 'lyftguy_text_75.png'); ?></p>
         <p>Copyright © 2016-2017 The LyftGuy. All Rights Reserved.</p>
+        <p class="small"><a href="/archive-index">Archive Index</a> | <a href="mailto:shawn@thelyftguy.net">Contact</a> | <a href="/subscribe">Subscribe</a></p>
+        <p><a href="https://www.facebook.com/theLyftGuy/" class="social-footer footer-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a> <a href="https://www.facebook.com/theLyftGuy/" class="social-footer footer-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></p>
     </footer>
 
     <?php wp_footer(); ?>
