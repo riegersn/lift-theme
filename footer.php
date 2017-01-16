73059b17549897168796aa1dc6cb5927e1f98237
riegersn <riegersn@gmail.com>
2017-01-16 11:10:25 -0500

    use id

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     </div><!-- /.m-container -->
 
-    <footer class="m-footer top-shadow">
+    <footer id="footer" class="top-shadow">
         <p><?php echo print_svg_image('footer-logo', 'lyftguy_text_75.svg', 'lyftguy_text_75.png'); ?></p>
         <p>Copyright © 2016-2017 The LyftGuy. All Rights Reserved.</p>
         <p class="small"><a href="/about">About</a>, <a href="/archive-index">Archive</a>, <a data-name="shawn" data-domain="thelyftguy" data-tld="net" href="#" class="cryptedmail" onclick="window.location.href = 'mailto:' + this.dataset.name + '@' + this.dataset.domain + '.' + this.dataset.tld">Contact</a>, <a href="/subscribe">Subscribe</a></p>
