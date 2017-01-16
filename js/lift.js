39bbcd5bdbf51577b8feb66d57251972993609fe
Shawn Rieger <riegersn@gmail.com>
2017-01-16 16:51:37 -0500

    refactor

--- js/lift.js
+++ js/lift.js
@@ -6,41 +6,20 @@ jQuery(document).ready(function($) {
         isAnimated: false
     });
 
-    $('#menu-item-365').click(function(event) {
-        var ww = $('.welcome-wrap'),
-            hleft = document.getElementsByClassName('header-wrap')[0].getBoundingClientRect().left + 20;
-
-        ww.finish();
-
-        if (ww.hasClass('toggled')) {
-            ww.removeClass('toggled');
-            ww.animate({'right': '-330'}, 'normal');
-        }
-        else {
-            ww.addClass('toggled');
-            ww.animate({'right': hleft}, 'normal');
-        }
-    });
-
-    //move the welcome/subscribe box
-    if (document.location.pathname == '/') {
-        var hleft = document.getElementsByClassName('header-wrap')[0].getBoundingClientRect().left + 20;
-        $('.welcome-wrap').css({'right': hleft}).addClass('toggled');
-    }
-
     // mobile nav menu, slide on click
-    $('.mobile-bars').click(function() {
-        var menu = $('.header-menu'),
+    $('.main-nav--button').click(function() {
+        var menu = $('.main-nav'),
+            button = $('.main-nav--button'),
             win_width = $(window).width();
 
         if (!menu.hasClass('active')) {
             menu.removeClass('nav-item-highlight').addClass('active');
             menu.css('right', '0');
-            $('.mobile-bars i').removeClass('fa-bars').addClass('fa-times');
+            button.removeClass('fa-bars').addClass('fa-times');
         } else {
             menu.css('right', '-' + win_width + 'px');
             menu.removeClass('active').addClass('nav-item-highlight');
-            $('.mobile-bars i').removeClass('fa-times').addClass('fa-bars');
+            button.removeClass('fa-times').addClass('fa-bars');
         }
     });
 
@@ -49,13 +28,13 @@ jQuery(document).ready(function($) {
 
         //move the welcome/subscribe box
         if ($('.welcome-wrap').is(':visible')) {
-            var hleft = document.getElementsByClassName('header-wrap')[0].getBoundingClientRect().left + 20;
+            var hleft = document.getElementsByClassName('container')[0].getBoundingClientRect().left + 20;
             $('.welcome-wrap').css({right: hleft});
         }
 
-        if ($(window).width() >= 860) {
+        if ($(window).width() >= 850) {
             console.log('testing');
-            $('.header-menu').css({
+            $('.main-nav').css({
                 display: 'block',
                 right: ''
             });
@@ -152,16 +131,22 @@ jQuery(document).ready(function($) {
         /* --- Freeze Shareable Container on Scroll --- */
         $(window).scroll(function() {
 
-            var fp_start,
+            var offset,
+                fp_start,
                 fp_stop,
-                sb = $('.sb-content-wrap'),
-                post = $('.post-content'),
-                content = $('.content-container:first'),
-                scroll_top = $(window).scrollTop();
+                header_h    = $('#header').height(),
+                padding     = 20,
+                margin      = 40,
+                post        = $('.post-content'),
+                scroll_t    = $(window).scrollTop(),
+                shareable   = $('#shareable-box'),
+                shareable_t = document.getElementById('shareable-box').getBoundingClientRect().top,
+                offset_t    = header_h + margin,
+                offset_b    = margin * 2 + header_h;
 
             if ($('.mobile-bars').is(':visible')) {
 
-                sb.css({ 'bottom': 0, 'left': 0 });
+                shareable.css({ 'bottom': 0, 'left': 0 });
 
                 // post.top - skinny.header.height + padding
                 fp_start = post.position().top - 66 + 30;
@@ -169,46 +154,30 @@ jQuery(document).ready(function($) {
                 // fp_start + post.height - window.height/2
                 fp_stop = fp_start + post.height() - ($(window).height() / 2);
 
-                if (scroll_top >= fp_start && scroll_top < fp_stop)
-                    sb.css('display', 'inline-block');
+                if (scroll_t >= fp_start && scroll_t < fp_stop)
+                    shareable.css('display', 'inline-block');
                 else
-                    sb.css('display', 'none');
+                    shareable.css('display', 'none');
 
             } else {
 
-                var sbv_height = sb.height(),
-                    content_t = content.offset().top,
-                    content_h = content.height();
+                fp_start = post.position().top - offset_t;
+                fp_stop = (post.height() + post.position().top) - shareable.height() - offset_b;
 
-                // console.log(scroll_top + '/' + content_t);
-
-                fp_start = content_t;
-                fp_stop = content_t + content_h - sbv_height;
-
-                // console.log(scroll_top + '/ ' + content_t + '+' + content_h + '-' + sbv_height + ' = ' + fp_stop);
-
-                if (scroll_top >= fp_start && scroll_top < (fp_stop - 70)) {
-
-                    // console.log('IF: ' + scroll_top + '/ ' + content_t + '+' + content_h + '-' + sbv_height + ' = ' + fp_stop);
-
-                    // freeze the container when scroll reaches the defined freeze pos
-                    if (!sb.hasClass('freeze')) {
-                        sb.css({ 'top': 40, 'position': 'fixed' }).addClass('freeze');
+                if (scroll_t >= fp_start && scroll_t < fp_stop) {
+                    if (!shareable.hasClass('freeze')) {
+                        shareable.css({ 'top': offset_t, 'position': 'fixed' }).addClass('freeze');
                     }
 
-                } else if (scroll_top >= fp_stop - 70) {
-
-                    // console.log('IF ELSE: ' + scroll_top + '/ ' + content_t + '+' + content_h + '-' + sbv_height + ' = ' + fp_stop);
-
+                } else if (scroll_t >= fp_stop - 70) {
                     // unfreeze the container when scroll reaches the defined freeze pos
-                    if (sb.hasClass('freeze'))
-                        sb.removeClass('freeze').css({ 'position': 'absolute', 'top': fp_stop - 128 });
+                    if (shareable.hasClass('freeze'))
+                        shareable.removeClass('freeze').css({ 'position': 'absolute', 'top': fp_stop + offset_t });
 
                 } else {
-                    // console.log('ELSE: ' + scroll_top + '/ ' + content_t + '+' + content_h + '-' + sbv_height + ' = ' + fp_stop);
                     // return to normal pos
-                    sb.css({ 'top': '', 'position': '' });
-                    sb.removeClass('freeze');
+                    shareable.css({ 'top': '', 'position': '' });
+                    shareable.removeClass('freeze');
                 }
             }
         });
