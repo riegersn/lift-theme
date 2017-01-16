64526340c85e940b97401499f5f4b0350629f507
Shawn Rieger <riegersn@gmail.com>
2017-01-15 19:27:42 -0500

    added about link, hide email

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <footer class="m-footer top-shadow">
         <p><?php echo print_svg_image('footer-logo', 'lyftguy_text_75.svg', 'lyftguy_text_75.png'); ?></p>
         <p>Copyright © 2016-2017 The LyftGuy. All Rights Reserved.</p>
-        <p class="small"><a href="/archive-index">Archive Index</a> | <a href="mailto:shawn@thelyftguy.net">Contact</a> | <a href="/subscribe">Subscribe</a></p>
+        <p class="small"><a href="/about">About</a>, <a href="/archive-index">Archive</a>, <a data-name="shawn" data-domain="thelyftguy" data-tld="net" href="#" class="cryptedmail" onclick="window.location.href = 'mailto:' + this.dataset.name + '@' + this.dataset.domain + '.' + this.dataset.tld">Contact</a>, <a href="/subscribe">Subscribe</a></p>
         <p><a href="https://www.facebook.com/theLyftGuy/" class="social-footer footer-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a> <a href="https://www.facebook.com/theLyftGuy/" class="social-footer footer-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></p>
     </footer>
 
