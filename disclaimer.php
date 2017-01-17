b181a5d4af1da9f3c70a59ba8976cd1a50d521f0
riegersn <riegersn@gmail.com>
2017-01-17 12:10:01 -0500

    disclaimer page

---
+++ disclaimer.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Disclaimer
+*/
+get_header(); ?>
+
+<div class="container">
+
+	<h1>Disclosure</h1>
+
+	<p>Content</p>
+
+</div><!-- #container -->
+
+<?php get_footer(); ?>
