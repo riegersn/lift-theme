26342299540da60f00bedc13f04032c01aad94e4
Shawn Rieger <riegersn@gmail.com>
2017-01-17 13:50:23 -0500

    added disclaimer link

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <footer id="footer" class="top-shadow">
         <p><?php echo print_svg_image('footer-logo','lyftguy_text_75.svg','lyftguy_text_75.png'); ?></p>
         <p>Copyright © 2016-2017 The LyftGuy. All Rights Reserved.</p>
-        <p><a href="/about">About</a><a href="/archive-index">Archive</a><a data-name="shawn" data-domain="thelyftguy" data-tld="net" href="#" class="cryptedmail" onclick="window.location.href = 'mailto:' + this.dataset.name + '@' + this.dataset.domain + '.' + this.dataset.tld">Contact</a><a href="/subscribe">Subscribe</a></p>
+        <p><a href="/about">About</a><a href="/archive-index">Archive</a><a data-name="shawn" data-domain="thelyftguy" data-tld="net" href="#" class="cryptedmail" onclick="window.location.href = 'mailto:' + this.dataset.name + '@' + this.dataset.domain + '.' + this.dataset.tld">Contact</a><a href="/disclaimer">Disclaimer</a><a href="/subscribe">Subscribe</a></p>
         <p>
         	<a href="https://www.facebook.com/theLyftGuy/" class="footer-social facebook">
         		<i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
