000868e6b454e52526861a2439723275dec7321b
riegersn <riegersn@gmail.com>
2017-01-17 00:02:16 -0500

    footer refactor

--- footer.php
+++ footer.php
@@ -1,10 +1,17 @@
     </div><!-- /.m-container -->
 
     <footer id="footer" class="top-shadow">
-        <p><?php echo print_svg_image('footer-logo', 'lyftguy_text_75.svg', 'lyftguy_text_75.png'); ?></p>
+        <p><?php echo print_svg_image('footer-logo','lyftguy_text_75.svg','lyftguy_text_75.png'); ?></p>
         <p>Copyright © 2016-2017 The LyftGuy. All Rights Reserved.</p>
-        <p class="small"><a href="/about">About</a>, <a href="/archive-index">Archive</a>, <a data-name="shawn" data-domain="thelyftguy" data-tld="net" href="#" class="cryptedmail" onclick="window.location.href = 'mailto:' + this.dataset.name + '@' + this.dataset.domain + '.' + this.dataset.tld">Contact</a>, <a href="/subscribe">Subscribe</a></p>
-        <p><a href="https://www.facebook.com/theLyftGuy/" class="social-footer footer-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a> <a href="https://www.facebook.com/theLyftGuy/" class="social-footer footer-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></p>
+        <p><a href="/about">About</a><a href="/archive-index">Archive</a><a data-name="shawn" data-domain="thelyftguy" data-tld="net" href="#" class="cryptedmail" onclick="window.location.href = 'mailto:' + this.dataset.name + '@' + this.dataset.domain + '.' + this.dataset.tld">Contact</a><a href="/subscribe">Subscribe</a></p>
+        <p>
+        	<a href="https://www.facebook.com/theLyftGuy/" class="footer-social facebook">
+        		<i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+        	</a>
+        	<a href="https://www.facebook.com/theLyftGuy/" class="footer-social twitter">
+        		<i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+        	</a>
+        </p>
     </footer>
 
     <?php wp_footer(); ?>
