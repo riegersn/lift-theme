2a763f3c12184c76983aa6e98fe7cfedd358e671
Shawn Rieger <riegersn@gmail.com>
2017-01-19 13:25:23 -0500

    fix social links in footer
    twitter link was invalid, make sure links opens in new window

--- footer.php
+++ footer.php
@@ -5,10 +5,10 @@
         <p>Copyright © 2016-2017 The LyftGuy. All Rights Reserved.</p>
         <p><a href="/about">About</a><a href="/archive-index">Archive</a><a data-name="shawn" data-domain="thelyftguy" data-tld="net" href="#" class="cryptedmail" onclick="window.location.href = 'mailto:' + this.dataset.name + '@' + this.dataset.domain + '.' + this.dataset.tld">Contact</a><a href="/disclaimer">Disclaimer</a><a href="/subscribe">Subscribe</a></p>
         <p>
-        	<a href="https://www.facebook.com/theLyftGuy/" class="footer-social facebook">
+        	<a href="https://www.facebook.com/theLyftGuy/" class="footer-social facebook" target="_blank">
         		<i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
         	</a>
-        	<a href="https://www.facebook.com/theLyftGuy/" class="footer-social twitter">
+        	<a href="https://twitter.com/thelyftguy_net" class="footer-social twitter" target="_blank">
         		<i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
         	</a>
         </p>
